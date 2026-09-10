a6f174321178836e7dd6567ce0de048369ac07ca
Tokute <nedbasilio@gmail.com>
2026-09-10 21:34:24 +0800

    Added file written output.

--- DSAL/Week14/Week14_Basilio_InsertionSortLab_Partner.py
+++ DSAL/Week14/Week14_Basilio_InsertionSortLab_Partner.py
@@ -22,7 +22,13 @@ def print_arr(arr):
         print(num, end=" ")
     print()
 
+def write_arr(arr):
+    for num in arr:
+        file.write(f"{num} ")
+    file.write("\n")
+
 if __name__ == "__main__":
+    file = open("output.txt", "w")
     while True:
         print("=" * 5, "Insertion Sort Program", "=" * 5)
         print("[1] Generate 10 Random Numbers")
@@ -39,16 +45,24 @@ if __name__ == "__main__":
             case 1:
                 arr = [random.randint(1, 25) for _ in range(10)]
                 print("======= Generated List =======")
+                file.write("======= Generated List =======\n")
                 print_arr(arr)
+                write_arr(arr)
             case 2:
                 insertion_sort(arr)
                 print("======= Sorted List (Ascending) =======")
+                file.write("======= Sorted List (Ascending) =======\n")
                 print_arr(arr)
+                write_arr(arr)
             case 3:
                 insertion_sort(arr, ascending=False)
                 print("======= Sorted List (Descending) =======")
+                file.write("======= Sorted List (Descending) =======\n")
                 print_arr(arr)
+                write_arr(arr)
             case 4:
                 print("Thank you for using the program.")
                 print("Ned Markus S. Basilio | CS-201")
                 break
+
+    file.close()
